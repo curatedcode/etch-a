c8f2d68c46a7c03f41e91967dde480d4c5bcece8
zack <zackaryfotheringham@hotmail.com>
2022-06-18 07:52:22 -0400

    final stage complete

--- main.js
+++ main.js
@@ -1,31 +1,76 @@
-document.body.onload = allDivs;
 
-function allDivs() {
-    const mainDiv = document.createElement('div');
-    mainDiv.id = 'main1';
-    mainDiv.classList.add('container');
-    const mDContent = document.createTextNode('');
-    mainDiv.appendChild(mDContent);
-    const currentDiv = document.getElementById('main1');
-    document.body.insertBefore(mainDiv,currentDiv);
-    let i=0;
-    while(i<256) {
-        let smallDiv,bDContent;
-        smallDiv = document.createElement('div');
-        smallDiv.id = 'babyDiv1';
-        smallDiv.classList.add('starter');
-        bDContent = document.createTextNode('');
-        smallDiv.append(bDContent);
-        mainDiv.appendChild(smallDiv);
-        
-        ++i;
-    };
+
+const mainBlock = document.getElementById('main-block');
+
+const containerSize = 600;
+const defaultSize = 16;
+const defaultColor = '#F7F2F1';
+
+let gridSize = defaultSize;
+let gridColor = defaultColor;
+
+
+function createGrid(){
+    let gridFullSize = gridSize*gridSize;
+    let gridPixelSize = containerSize/(gridFullSize/gridSize);
+    console.log(gridPixelSize);
+    const containerDiv = document.createElement('div');
+    containerDiv.id = 'containerDiv';
+    containerDiv.classList.add('containerDiv');
+    mainBlock.appendChild(containerDiv);
+
+    for (let i=0;i<gridFullSize;++i){
+        const gridDiv = document.createElement('div');
+        gridDiv.id = 'gridDiv';
+        gridDiv.style.width = `${gridPixelSize}px`;
+        gridDiv.style.height = `${gridPixelSize}px`;
+        gridDiv.addEventListener('mousedown', updateColor); 
+        gridDiv.addEventListener('mouseover', updateColor);
+        containerDiv.appendChild(gridDiv);
+    }
 };
 
-const container = document.querySelector('.container');
-container.addEventListener('mouseenter', e => {
-    let target = e.target;
-    if (target !== container) {
-        target.classList.add('babyStyle');
+let mouseDown = false;
+
+document.onmousedown = function (){
+    mouseDown = true;
+};
+
+document.onmouseup = function (){
+    mouseDown = false;
+};
+
+function updateColor(e){
+    if (e.type === 'mouseover' && mouseDown == true){
+        e.target.style.backgroundColor = 'black';
+    } 
+};
+
+const button = document.querySelector('button');
+const textBox = document.querySelector('input');
+
+button.addEventListener('click',updateGridSize);
+
+
+function updateGridSize() {
+    if (textBox.value === '' || isNaN(textBox.value)) {
+        alert('Please enter a number');
+    } else {
+        gridSize = textBox.value;
+        const mainBlock = document.getElementById('main-block');
+        const gridContainer = document.getElementById('containerDiv');
+        while (gridContainer.firstChild){
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+        mainBlock.removeChild(mainBlock.lastChild);
+        textBox.value='';
+
+        createGrid();
+        console.log(textBox.value);
     }
-});
+};
+
+window.onload = createGrid;
+
+
+
